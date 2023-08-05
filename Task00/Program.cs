69b22650fb71739aa69c3609e553f9a775363683
nikitakudko <nikitakudko@gmail.com>
2023-08-05 23:41:58 +0300

    Выполнил task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.Write("Введите ваше число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sqr = number * number;
+Console.Write("квадрат числа равен ");
+Console.WriteLine(sqr);
