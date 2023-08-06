92264af4b25a15026f8b7fb01754615856f2511c
nikitakudko <nikitakudko@gmail.com>
2023-08-06 10:46:56 +0300

    Дополнил решение

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,5 +1,4 @@
 ﻿Console.Write("Введите ваше число ");
 int number = Convert.ToInt32(Console.ReadLine());
 int sqr = number * number;
-Console.Write("квадрат числа равен ");
-Console.WriteLine(sqr);
+Console.Write($"квадрат числа {number} = {sqr}");
