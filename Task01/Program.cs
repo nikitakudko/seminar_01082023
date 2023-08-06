5b49f52c2d273f2eb105735bd8699cf3179a0805
nikitakudko <nikitakudko@gmail.com>
2023-08-06 10:45:40 +0300

    Добавил решение Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите целое число А");
+int number_A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое число B");
+int number_B = Convert.ToInt32(Console.ReadLine());
+
+if(number_A / number_B == number_B)
+{
+    Console.WriteLine($"Число {number_A}  - квадрат числа {number_B}");
+}
+else
+{
+    Console.WriteLine($"Число {number_A} не является квадратом числа {number_B}");
+}
